3d21e210327d0207120a5dfbc894fc3e7f8a372c
taibihakim2002 <taibihakim2002@gmail.com>
2024-03-18 18:05:26 +0100

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dawid-food-ordering.s3.amazonaws.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
